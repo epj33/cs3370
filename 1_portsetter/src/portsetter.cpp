07e0e79c5eb0087e9a8c56d9d3dbf7f533287ca1
ericj <epjmail@gmail.com>
2016-08-30 04:47:02 +0000

    restructure of cs3370 repo

---
+++ 1_portsetter/src/portsetter.cpp
@@ -0,0 +1,13 @@
+/**********************************************************
+cs3370 fall 2016
+eric jorgensen
+assn1: portsetter
+**********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}//end fx main
