8e73bf3a02b1ecd5747d06095b545e7193877ee0
ericj <epjmail@gmail.com>
2016-09-09 04:32:08 +0000

    adding new files for CO1

---
+++ 1_portsetter/runT.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#run the command
+bin/testport
+echo 'return code' $?
---
+++ 1_portsetter/src/test.cpp
@@ -0,0 +1,21 @@
+/**********************************************************
+cs3370 fall 2016
+eric jorgensen
+assn1: portsetter
+use echo $? to show exit code.
+**********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+/**********************************************************
+ * input params:    -h, --help
+ *                  -p <portNumber>, --port <portNumber>
+ * 
+ * output expected: "listening on port <portFromCmdLine>"
+ * 
+**********************************************************/
+int main(int argc, char *args[]) {
+    cout << "hello test" << endl;
+}//end fx main
