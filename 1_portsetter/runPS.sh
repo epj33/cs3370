b1c7a8453c1cb57cdc99e20b9abe09d245b69631
ericj <epjmail@gmail.com>
2016-09-13 14:50:13 +0000

    linux ready, almost. now win.

--- 1_portsetter/runPS.sh
+++ 1_portsetter/runPS.sh
@@ -6,14 +6,12 @@ NEWEXEC="setport"
 echo $DIRE
 if [[ -n $(find . -name "setport") ]] 
 then
-    echo "found it"
-# use : for do nothing    
+    : 
 else
     echo "not found"
     $(mv $DIRE$EXEC $DIRE/$NEWEXEC)
-    $(echo PATH=$PATH:$DIRE >> ~/.bashrc)
-    $(source ~/.bashrc)
+    $(source PATH=$PATH:$DIRE)
 fi
 
-$DIRE/$NEWEXEC $1 $2 $3
+$NEWEXEC $1 $2 $3
 echo 'return code' $?
